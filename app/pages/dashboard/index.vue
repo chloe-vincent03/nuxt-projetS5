5fa4ad9fd7298718cf5ce55eac72e111a54713c3
Chloé Vincent <chloe.vincent03@edu.univ-fcomte.fr>
2025-12-03 14:10:39 +0100

    feat(gestionR) ajout des cards dans le dashboard

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,29 +1,47 @@
-<script setup lang="ts" >
+<script setup lang="ts">
 definePageMeta({
   middleware: ['auth']
 })
 
+const config = useRuntimeConfig()
+
+// Récupération des recettes de l'utilisateur connecté
+const { data: myRecipes, error } = await useAsyncData('my-recipes', async () => {
+  const cookie = useCookie('recipe_token')
+  return await $fetch<ApiResponse<Recipe[]>>(`${config.public.apiUrl}/api/recipes/my-recipes`, {
+    headers: {
+      Authorization: `Bearer ${cookie.value}`
+    }
+  }).then(res => res.data)
+})
 
 async function logout () {
   const cookie = useCookie('recipe_token')
   cookie.value = null
   navigateTo('/login')
 }
-const config = useRuntimeConfig()
 
-await useAsyncData('my-recipes', () => {
-  const cookie = useCookie('recipe_token')
-  return $fetch(`${config.public.apiUrl}/api/recipes/my-recipes`,{
-    headers :{
-      Authorization: `Bear ${cookie.value}`
-    }
-  })
-})
+if (error && error.value) {
+  throw new Error('Impossible de charger vos recettes.')
+}
 </script>
 
 <template>
-  <div class="p-dashboard" >
+  <div class="p-dashboard">
     <h1>Dashboard</h1>
     <MButton type="button" @click="logout">Se déconnecter</MButton>
+
+    <h2 class="mt-6">Mes recettes :</h2>
+
+    <div v-if="myRecipes" class="recipes-grid" >
+      <div
+        v-for="(recipe, index) in myRecipes"
+        :key="index"
+      >
+        <MCards :recipe="recipe" size="small" />
+      </div>
+    </div>
+
+    <p v-else>Aucune recette trouvée.</p>
   </div>
 </template>
